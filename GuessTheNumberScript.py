f92d0369c43dfbd6bcf3c299c28d78dfdb752ae3
lekanoni20 <lekanoni18@yahoo.co.uk>
2020-05-23 20:44:52 +0100

    Guess the number script updated

--- GuessTheNumberScript.py
+++ GuessTheNumberScript.py
@@ -6,5 +6,7 @@ ComputerValue = random.randint(0, 20)
 
 if ComputerValue == pick:
     print("You guessed right!!!!! Wow!!!")
-else:
-    print("Your guess is wrong. The correct number is " + str(ComputerValue) + ". Try again!!!")
+elif ComputerValue > pick:
+    print("Your number is too low. The correct number is " + str(ComputerValue) + ". Try again!!!")
+elif pick > ComputerValue:
+    print("Your number is too high. The correct number is " + str(ComputerValue) + ". Try again!!!")
